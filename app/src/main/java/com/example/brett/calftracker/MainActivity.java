d915e7ff10497fac277c3a8749e6f430435ff25c
Brett percy <brettpercy01@gmail.com>
2017-10-24 21:15:16 -0500

    Brett commit

--- app/src/main/java/com/example/brett/calftracker/MainActivity.java
+++ app/src/main/java/com/example/brett/calftracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.brett.calftracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Merged comment - Brett
 public class MainActivity extends AppCompatActivity {
 
     @Override
