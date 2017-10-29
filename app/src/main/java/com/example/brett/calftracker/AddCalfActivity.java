f78168199cad126e71e483389c9e439e2b4e6235
JT Schelling <jtschelling@wisc.edu>
2017-10-29 17:54:29 -0500

    Layout of AddCalfActivity complete, all fields are ready to accept user input but havent implemented the "add" button to actually create a new calf object.

--- app/src/main/java/com/example/brett/calftracker/AddCalfActivity.java
+++ app/src/main/java/com/example/brett/calftracker/AddCalfActivity.java
@@ -1,13 +1,81 @@
 package com.example.brett.calftracker;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddCalfActivity extends AppCompatActivity {
+    private static final String TAG = "AddCalfActivity";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    private TextView mGender;
+    private Dialog mGenderListDialog;
+    private String[] gender = {"Male","Female"};
+    private AlertDialog alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_calf);
+
+        mDisplayDate = (TextView) findViewById(R.id.textViewDisplayDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddCalfActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+
+        mGender = (TextView) findViewById(R.id.textViewSelectGender);
+
+        mGender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alert.show();
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Gender");
+        builder.setItems(gender, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                mGender.setText(gender[i]);
+            }
+        });
+        alert = builder.create();
     }
 }
--- app/src/main/java/com/example/brett/calftracker/MainActivity.java
+++ app/src/main/java/com/example/brett/calftracker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.brett.calftracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -14,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickButton(View view) {
+        Intent intent = new Intent(this,AddCalfActivity.class);
+        startActivity(intent);
     }
 }
