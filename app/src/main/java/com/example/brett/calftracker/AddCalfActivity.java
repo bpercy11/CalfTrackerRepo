03746995b6fdf0a3daba4de7e7168988ec4f174b
JT Schelling <jtschelling@wisc.edu>
2017-10-31 22:36:01 -0500

    fixed the thing where addcalf to a new arraylist would break it. it now checks to see if theres an existing sharedpreference file for the arraylist

--- app/src/main/java/com/example/brett/calftracker/AddCalfActivity.java
+++ app/src/main/java/com/example/brett/calftracker/AddCalfActivity.java
@@ -51,15 +51,16 @@ public class AddCalfActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_calf);
 
-        calfList = new ArrayList<Calf>();
-
         // Load the calfList
-//        SharedPreferences mPreferences = getSharedPreferences("CalfTracker", Activity.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = mPreferences.edit();
-//
-//        Gson gson = new Gson();
-//        String json = mPreferences.getString("CalfList","");
-//        calfList = gson.fromJson(json, new TypeToken<ArrayList<Calf>>(){}.getType());
+        SharedPreferences mPreferences = getSharedPreferences("CalfTracker", Activity.MODE_PRIVATE);
+        if(mPreferences.contains("CalfList")) {
+            SharedPreferences.Editor editor = mPreferences.edit();
+
+            Gson gson = new Gson();
+            String json = mPreferences.getString("CalfList", "");
+            calfList = gson.fromJson(json, new TypeToken<ArrayList<Calf>>() {
+            }.getType());
+        } else { calfList = new ArrayList<Calf>(); }
 
         // Sets the default date to be today in case this field is left blank
         Calendar today = Calendar.getInstance();
