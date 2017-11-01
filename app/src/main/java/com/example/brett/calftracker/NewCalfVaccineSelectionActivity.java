bf1f324cd584aa169620324b5031ff37723ff986
JT Schelling <jtschelling@wisc.edu>
2017-11-01 15:22:47 -0500

    See last committ message, I didn't add the new files apparently.

---
+++ app/src/main/java/com/example/brett/calftracker/NewCalfVaccineSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.example.brett.calftracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCalfVaccineSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_calf_vaccine_selection);
+    }
+}
