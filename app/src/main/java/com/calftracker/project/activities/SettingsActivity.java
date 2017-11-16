a863d162c02e2d345c776c569c943bd06e85bd2c
Brett percy <brettpercy01@gmail.com>
2017-11-16 17:54:16 -0600

    adding SettingsActivity file

---
+++ app/src/main/java/com/calftracker/project/activities/SettingsActivity.java
@@ -0,0 +1,32 @@
+package com.calftracker.project.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.calftracker.project.calftracker.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+    }
+}
