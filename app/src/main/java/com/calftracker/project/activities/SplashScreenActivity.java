01d935469a78fd1a3a31917debcc750e8ca4b0d1
bjlapp <bjlapp33@gmail.com>
2017-11-20 19:56:55 -0600

    Updated Splash Screen

--- app/src/main/java/com/calftracker/project/activities/SplashScreenActivity.java
+++ app/src/main/java/com/calftracker/project/activities/SplashScreenActivity.java
@@ -1,16 +1,22 @@
 package com.calftracker.project.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.content.SharedPreferences;
+import android.os.SystemClock;
 
 import com.calftracker.project.calftracker.R;
+import com.calftracker.project.models.Firebase;
+import com.google.firebase.database.DatabaseReference;
 
 /**
  * Created by project on 10/31/17.
  */
 public class SplashScreenActivity extends Activity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +24,33 @@ public class SplashScreenActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+
+        Firebase test = (Firebase) getApplicationContext();
+
+        test.compareToFirebase();
+
+        //test.editSharedPreferences("usedApp", "false");
+        //test.editSharedPreferences("usedApp", "true");
+
+
         //final SharedPreferences sharedPref = getSharedPreferences("test",Activity.MODE_PRIVATE);
         //final SharedPreferences.Editor editor = sharedPref.edit();
         //editor.putBoolean("usedApp", false);
         //editor.commit();
 
-        Thread timerThread = new Thread(){
-            public void run(){
+        //Thread timerThread = new Thread(){
+            //public void run(){
+
+
                 try{
-                    sleep(3000);
+
+                    Thread.sleep(3000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
-
-                    SharedPreferences sharedPref = getSharedPreferences("CalfTracker",Activity.MODE_PRIVATE);
+                    test.editSharedPreferences("test", "test");
+                    SharedPreferences sharedPref = getSharedPreferences("usedApp",Activity.MODE_PRIVATE);
+                    //boolean hasBeenUsed = sharedPref.getBoolean("usedApp", false);
                     boolean hasBeenUsed = sharedPref.getBoolean("usedApp", false);
 
                     if(!hasBeenUsed) {
@@ -48,9 +67,9 @@ public class SplashScreenActivity extends Activity {
 
                     }
                 }
-            }
-        };
-        timerThread.start();
+            //}
+        //};
+        //timerThread.start();
     }
 
 
