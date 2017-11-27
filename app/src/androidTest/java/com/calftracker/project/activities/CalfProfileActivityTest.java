89dc788f02226801c356ede631b7e10aa714956c
Alayna Truttmann <atruttmann@wisc.edu>
2017-11-26 18:42:42 -0600

    More Calf Profile UI Tests

--- app/src/androidTest/java/com/calftracker/project/activities/CalfProfileActivityTest.java
+++ app/src/androidTest/java/com/calftracker/project/activities/CalfProfileActivityTest.java
@@ -1,34 +1,42 @@
 package com.calftracker.project.activities;
 
+import android.content.ComponentName;
+import android.provider.MediaStore;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.PickerActions;
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.widget.DatePicker;
 
 import com.calftracker.project.calftracker.R;
 
 import org.hamcrest.Matchers;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Calendar;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class CalfProfileActivityTest {
     @Rule
-    public ActivityTestRule<CalfProfileActivity> mActivityTestRule = new ActivityTestRule(CalfProfileActivity.class);
-    private CalfProfileActivity mActivity = null;
+    public IntentsTestRule<CalfProfileActivity> mIntentsTestRule = new IntentsTestRule(CalfProfileActivity.class);
 
-    @Before
-    public void setUp() throws Exception {
-        mActivity = mActivityTestRule.getActivity();
+    @Test
+    public void testEditAddPhoto() throws Exception {
+        onView(withId(R.id.floatingActionButtonEDIT)).perform(click());
+        onView(withId(R.id.buttonAddPhoto)).perform(click());
+        intended(hasAction(MediaStore.ACTION_IMAGE_CAPTURE));
     }
 
     @Test
@@ -106,8 +114,57 @@ public class CalfProfileActivityTest {
 
     }*/
 
-    @After
-    public void tearDown() throws Exception {
-        mActivity = null;
+    @Test
+    public void testRecordWeight() throws Exception {
+        String newWeight = "64.2";
+        onView(withId(R.id.buttonAddWeight)).perform(click());
+        onView(withId(R.id.dialogTextInput)).perform(ViewActions.typeText(newWeight), closeSoftKeyboard());
+        onView(withText("Ok")).perform(click());
+        onView(withId(R.id.textViewWeightValue)).check(matches(withText(newWeight + " lbs")));
+    }
+
+    @Test
+    public void testRecordHeight() throws Exception {
+        String newHeight = "54.3";
+        onView(withId(R.id.buttonAddHeight)).perform(click());
+        onView(withId(R.id.dialogTextInput)).perform(ViewActions.typeText(newHeight), closeSoftKeyboard());
+        onView(withText("Ok")).perform(click());
+        onView(withId(R.id.textViewHeightValue)).check(matches(withText(newHeight + " in")));
+    }
+
+    @Test
+    public void testOpenGrowthHistory() throws Exception {
+        onView(withId(R.id.buttonGrowthHistory)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), CalfProfileGrowthHistoryActivity.class)));
+    }
+
+    @Test
+    public void testOpenFeedingHistory() throws Exception {
+        onView(withId(R.id.buttonFeedingHistory)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), CalfProfileFeedingHistoryActivity.class)));
+    }
+
+    @Test
+    public void testOpenMedicalHistory() throws Exception {
+        onView(withId(R.id.buttonCreateNewNote)).perform(scrollTo());   // Have to do this to get around the FAB button
+        onView(withId(R.id.buttonMedicalHistory)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), CalfProfileMedicalHistoryActivity.class)));
+    }
+
+    @Test
+    public void testAddNote() throws Exception {
+        String newNote = "Test note";
+        Calendar currentDate = Calendar.getInstance();
+        int day = currentDate.get(Calendar.DAY_OF_MONTH);
+        int month = currentDate.get(Calendar.MONTH);
+        int year = currentDate.get(Calendar.YEAR);
+        String date = month + "/" + day + "/" + year;
+
+        onView(withId(R.id.buttonCreateNewNote))
+                .perform(scrollTo())
+                .perform(click());
+        onView(withId(R.id.dialogTextInput)).perform(ViewActions.typeText(newNote), closeSoftKeyboard());
+        onView(withText("Ok")).perform(click());
+        // TODO
     }
 }
