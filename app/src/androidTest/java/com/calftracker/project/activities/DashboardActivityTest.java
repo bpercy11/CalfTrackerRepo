30bab7c44cee3396a932fc60db5c1f0190a4cb3f
Alayna Truttmann <atruttmann@wisc.edu>
2017-11-27 11:25:07 -0600

    Added navigation drawer tests

--- app/src/androidTest/java/com/calftracker/project/activities/DashboardActivityTest.java
+++ app/src/androidTest/java/com/calftracker/project/activities/DashboardActivityTest.java
@@ -1,8 +1,11 @@
 package com.calftracker.project.activities;
 
 import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.view.Gravity;
 
 import com.calftracker.project.calftracker.R;
 
@@ -11,6 +14,8 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -20,6 +25,56 @@ import static org.hamcrest.Matchers.allOf;
 public class DashboardActivityTest {
     @Rule
     public IntentsTestRule<DashboardActivity> mIntentsTestRuleDashboard = new IntentsTestRule<DashboardActivity>(DashboardActivity.class);
+
+    @Test
+    public void testOpenCalfListMenu() throws Exception {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_list));
+        intended(hasComponent(CalfListActivity.class.getName()));
+    }
+
+    @Test
+    public void testOpenAddCalfMenu() throws Exception {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_add));
+        intended(hasComponent(AddCalfActivity.class.getName()));
+    }
+
+    @Test
+    public void testOpenTasksMenu() throws Exception {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_tasks));
+        intended(hasComponent(TasksActivity.class.getName()));
+    }
+
+    @Test
+    public void testOpenProtocolsMenu() throws Exception {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_protocols));
+        intended(hasComponent(VaccineActivity.class.getName()));
+    }
+
+    @Test
+    public void testOpenSettingsMenu() throws Exception {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_settings));
+        intended(hasComponent(SettingsActivity.class.getName()));
+    }
 
     @Test
     public void testOpenCalfList() throws Exception {
