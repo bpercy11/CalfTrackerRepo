bf24df36369f40c4142b5cc21d2f00da70ea9f52
Brett percy <brettpercy01@gmail.com>
2017-12-04 19:24:54 -0600

    new DashboardActivity

--- app/src/main/java/com/calftracker/project/activities/DashboardActivity.java
+++ app/src/main/java/com/calftracker/project/activities/DashboardActivity.java
@@ -39,80 +39,64 @@ public class DashboardActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         getLayoutInflater().inflate(R.layout.activity_dashboard, frameLayout);
 
+        mNavigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+        GridView grid;
+        String[] dashboard_items = {
+                getString(R.string.calf_list_title),
+                getString(R.string.add_calf_title),
+                getString(R.string.tasks_title),
+                getString(R.string.protocols_title),
+                getString(R.string.insights_title),
+                getString(R.string.settings_title)
+        } ;
+        int[] imageId = {
+                R.drawable.ic_view_list_black_24dp,
+                R.drawable.ic_add_circle_black_24dp,
+                R.drawable.ic_check_box_black_24dp,
+                R.drawable.ic_local_hospital_black_24dp,
+                R.drawable.ic_insert_chart_black_24dp,
+                R.drawable.ic_settings_black_24dp
+        };
+        DashboardGridAdapter adapter = new DashboardGridAdapter(DashboardActivity.this, dashboard_items, imageId);
+        grid=(GridView)findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        //retrieveData();
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent intent;
+                switch(position) {
+                    case 0: intent = new Intent(DashboardActivity.this,CalfListActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1: intent = new Intent(DashboardActivity.this,AddCalfActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 2: intent = new Intent(DashboardActivity.this,TasksActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 3: intent = new Intent(DashboardActivity.this,VaccineActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 4: break;
+                    case 5: intent = new Intent(DashboardActivity.this,SettingsActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+
+
+        retrieveData();
 
         mFarmName = (TextView) findViewById(R.id.textViewFarmNameDashboard);
 
-        fb = (Firebase) getApplicationContext();
-        DatabaseReference root = FirebaseDatabase.getInstance().getReference();
-        fb.readData(root.child("farmName"), new Firebase.OnGetDataListener() {
-            @Override
-            public void onSuccess(DataSnapshot dataSnapshot) {
-                temp = (String) dataSnapshot.getValue();
-                mFarmName.setText(temp);
-            }
+        mFarmName.setText(farm.getName());
 
-            @Override
-            public void onStart() {
-                mNavigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
-                GridView grid;
-                String[] dashboard_items = {
-                        getString(R.string.calf_list_title),
-                        getString(R.string.add_calf_title),
-                        getString(R.string.tasks_title),
-                        getString(R.string.protocols_title),
-                        getString(R.string.insights_title),
-                        getString(R.string.settings_title)
-                } ;
-                int[] imageId = {
-                        R.drawable.ic_view_list_black_24dp,
-                        R.drawable.ic_add_circle_black_24dp,
-                        R.drawable.ic_check_box_black_24dp,
-                        R.drawable.ic_local_hospital_black_24dp,
-                        R.drawable.ic_insert_chart_black_24dp,
-                        R.drawable.ic_settings_black_24dp
-                };
-                DashboardGridAdapter adapter = new DashboardGridAdapter(DashboardActivity.this, dashboard_items, imageId);
-                grid=(GridView)findViewById(R.id.grid);
-                grid.setAdapter(adapter);
-                grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view,
-                                            int position, long id) {
-                        Intent intent;
-                        switch(position) {
-                            case 0: intent = new Intent(DashboardActivity.this,CalfListActivity.class);
-                                startActivity(intent);
-                                break;
-                            case 1: intent = new Intent(DashboardActivity.this,AddCalfActivity.class);
-                                startActivity(intent);
-                                break;
-                            case 2: intent = new Intent(DashboardActivity.this,TasksActivity.class);
-                                startActivity(intent);
-                                break;
-                            case 3: intent = new Intent(DashboardActivity.this,VaccineActivity.class);
-                                startActivity(intent);
-                                break;
-                            case 4: break;
-                            case 5: intent = new Intent(DashboardActivity.this,SettingsActivity.class);
-                                startActivity(intent);
-                                break;
-                        }
-                    }
-                });
-
-            }
-
-            @Override
-            public void onFailure() {
-                Log.d("ONFAILURE", "Failed");
-            }
-        });
     }
 
-    /*
+
     public void retrieveData() {
         // set up shared preference variables
         SharedPreferences mPreferences = getSharedPreferences("CalfTracker", Activity.MODE_PRIVATE);
@@ -123,7 +107,7 @@ public class DashboardActivity extends BaseActivity {
         json = mPreferences.getString("Farm", "");
         farm = gson.fromJson(json, new TypeToken<Farm>() {}.getType());
     }
-    */
+
 
     @Override
     public void onBackPressed() {
