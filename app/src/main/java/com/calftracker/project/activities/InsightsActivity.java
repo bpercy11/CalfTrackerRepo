388af5771c3eb8d0566e8fa7d21b3691c3b4a592
Alayna Truttmann <atruttmann@wisc.edu>
2017-12-06 01:41:05 -0600

    Added Calf DOB Line Chart

--- app/src/main/java/com/calftracker/project/activities/InsightsActivity.java
+++ app/src/main/java/com/calftracker/project/activities/InsightsActivity.java
@@ -10,16 +10,26 @@ import android.support.v4.widget.DrawerLayout;
 
 import com.calftracker.project.calftracker.R;
 import com.calftracker.project.models.Calf;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class InsightsActivity extends BaseActivity {
 
@@ -45,6 +55,7 @@ public class InsightsActivity extends BaseActivity {
 
         getGenderData();
         getHealthData();
+        getDOBData();
     }
 
     public void getGenderData() {
@@ -145,6 +156,176 @@ public class InsightsActivity extends BaseActivity {
         legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
         legend.setTextSize(16f);
         legend.setXEntrySpace(24f);
+    }
+
+    public void getDOBData() {
+        // Get dates in the last week
+        Calendar date1 = Calendar.getInstance();
+        date1.add(Calendar.DAY_OF_YEAR, -6);
+        int year1 = date1.get(Calendar.YEAR);
+        int month1 = date1.get(Calendar.MONTH) + 1;
+        int day1 = date1.get(Calendar.DATE);
+
+        Calendar date2 = Calendar.getInstance();
+        date2.add(Calendar.DAY_OF_YEAR, -5);
+        int year2 = date2.get(Calendar.YEAR);
+        int month2 = date2.get(Calendar.MONTH) + 1;
+        int day2 = date2.get(Calendar.DATE);
+
+        Calendar date3 = Calendar.getInstance();
+        date3.add(Calendar.DAY_OF_YEAR, -4);
+        int year3 = date3.get(Calendar.YEAR);
+        int month3 = date3.get(Calendar.MONTH) + 1;
+        int day3 = date3.get(Calendar.DATE);
+
+        Calendar date4 = Calendar.getInstance();
+        date4.add(Calendar.DAY_OF_YEAR, -3);
+        int year4 = date4.get(Calendar.YEAR);
+        int month4 = date4.get(Calendar.MONTH) + 1;
+        int day4 = date4.get(Calendar.DATE);
+
+        Calendar date5 = Calendar.getInstance();
+        date5.add(Calendar.DAY_OF_YEAR, -2);
+        int year5 = date5.get(Calendar.YEAR);
+        int month5 = date5.get(Calendar.MONTH) + 1;
+        int day5 = date5.get(Calendar.DATE);
+
+        Calendar date6 = Calendar.getInstance();
+        date6.add(Calendar.DAY_OF_YEAR, -1);
+        int year6 = date6.get(Calendar.YEAR);
+        int month6 = date6.get(Calendar.MONTH) + 1;
+        int day6 = date6.get(Calendar.DATE);
+
+        Calendar date7 = Calendar.getInstance();
+        int year7 = date7.get(Calendar.YEAR);
+        int month7 = date7.get(Calendar.MONTH) + 1;
+        int day7 = date7.get(Calendar.DATE);
+
+        String monthDay1 = month1 + "/" + day1;
+        String monthDay2 = month2 + "/" + day2;
+        String monthDay3 = month3 + "/" + day3;
+        String monthDay4 = month4 + "/" + day4;
+        String monthDay5 = month5 + "/" + day5;
+        String monthDay6 = month6 + "/" + day6;
+        String monthDay7 = month7 + "/" + day7;
+
+        int countDay1 = 0;
+        int countDay2 = 0;
+        int countDay3 = 0;
+        int countDay4 = 0;
+        int countDay5 = 0;
+        int countDay6 = 0;
+        int countDay7 = 0;
+
+        if (calfList != null) {
+            for (int i = 0; i < calfList.size(); i++) {
+                int calfYear = calfList.get(i).getDateOfBirth().get(Calendar.YEAR);
+                int calfMonth = calfList.get(i).getDateOfBirth().get(Calendar.MONTH) + 1;
+                int calfDay = calfList.get(i).getDateOfBirth().get(Calendar.DATE);
+
+                if (calfYear == year1 && calfMonth == month1 && calfDay == day1) {
+                    countDay1++;
+                }
+                else if (calfYear == year2 && calfMonth == month2 && calfDay == day2) {
+                    countDay2++;
+                }
+                else if (calfYear == year3 && calfMonth == month3 && calfDay == day3) {
+                    countDay3++;
+                }
+                else if (calfYear == year4 && calfMonth == month4 && calfDay == day4) {
+                    countDay4++;
+                }
+                else if (calfYear == year5 && calfMonth == month5 && calfDay == day5) {
+                    countDay5++;
+                }
+                else if (calfYear == year6 && calfMonth == month6 && calfDay == day6) {
+                    countDay6++;
+                }
+                else if (calfYear == year7 && calfMonth == month7 && calfDay == day7) {
+                    countDay7++;
+                }
+            }
+        }
+
+        ArrayList<Integer> counts = new ArrayList<>();
+        counts.add(countDay1);
+        counts.add(countDay2);
+        counts.add(countDay3);
+        counts.add(countDay4);
+        counts.add(countDay5);
+        counts.add(countDay6);
+        counts.add(countDay7);
+
+        int maxCount = 0;
+        for (int i = 0; i < counts.size(); i++) {
+            if (counts.get(i) > maxCount) {
+                maxCount = counts.get(i);
+            }
+        }
+
+        final HashMap<Integer, String> numMap = new HashMap<>();
+        numMap.put(1, monthDay1);
+        numMap.put(2, monthDay2);
+        numMap.put(3, monthDay3);
+        numMap.put(4, monthDay4);
+        numMap.put(5, monthDay5);
+        numMap.put(6, monthDay6);
+        numMap.put(7, monthDay7);
+
+        LineChart dobChart = (LineChart) findViewById(R.id.dobChart);
+        dobChart.getDescription().setEnabled(false);
+        dobChart.setTouchEnabled(false);
+        dobChart.setExtraOffsets(0,-10,0,10);
+
+        XAxis xAxis = dobChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setYOffset(10);
+        xAxis.setAxisMinimum(1);
+        xAxis.setAxisMaximum(7);
+        xAxis.setTextSize(10f);
+        xAxis.setValueFormatter(new DefaultAxisValueFormatter(0) {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return numMap.get((int)value);
+            }
+        });
+
+        dobChart.getAxisRight().setEnabled(false);
+        YAxis yAxis = dobChart.getAxisLeft();
+        yAxis.setGranularity(1f);
+        yAxis.setAxisMinimum(0);
+        yAxis.setAxisMaximum(maxCount);
+        yAxis.setTextSize(10f);
+        yAxis.setValueFormatter(new DefaultAxisValueFormatter(0) {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return "" + ((int) value);
+            }
+        });
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, countDay1));
+        entries.add(new Entry(2, countDay2));
+        entries.add(new Entry(3, countDay3));
+        entries.add(new Entry(4, countDay4));
+        entries.add(new Entry(5, countDay5));
+        entries.add(new Entry(6, countDay6));
+        entries.add(new Entry(7, countDay7));
+
+        LineDataSet dataSet = new LineDataSet(entries, "");
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+        colors.add(getResources().getColor(R.color.colorLinePurple));
+        dataSet.setColors(colors);
+        dataSet.setCircleColor(getResources().getColor(R.color.colorLinePurple));
+        dataSet.setCircleRadius(4);
+        dataSet.setDrawValues(false);
+        dataSet.setLineWidth(2);
+
+        LineData data = new LineData(dataSet);
+        dobChart.setData(data);
+
+        Legend legend = dobChart.getLegend();
+        legend.setEnabled(false);
     }
 
     @Override
